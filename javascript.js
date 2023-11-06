e57385af0e462d846d2e5dd0e661c6ecc431e9bc
Ferdous <71073170+alexinferdous@users.noreply.github.com>
2023-11-05 22:55:44 -0800

    Container is centered with correct cell sizing. Need to add reset button next

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
 const containerJS = document.getElementById("container");
 
-containerJS.style.border = 0; 
+
+//defining the size and border of the container
+containerJS.style.border = "1px solid #333"; 
 containerJS.style.padding = 0;
 containerJS.style.margin = 0;
-containerJS.style.height = '700px';
-containerJS.style.width = '700px';
+containerJS.style.height = '672px';
+containerJS.style.width = '672px';
+
+
+// centering the container
+const left1 = (window.innerWidth - containerJS.offsetWidth) / 2;
+const top1 = (window.innerHeight - containerJS.offsetHeight) / 2;
+containerJS.style.left = left1 + 'px';
+containerJS.style.top = top1 + 'px';
 
 
 containerJS.style.display = "flex";
-//containerJS.style.alignItems = "center"; // or any other value
 containerJS.style.flexWrap = "wrap"; // or "nowrap" to prevent wrapping 
 
 
@@ -20,10 +28,13 @@ for(let i = 0; i <=255; i++){
     gridCell.classList.add("gridClass");
     gridCell.setAttribute('style', `border:1px dotted #808080`);
     
+    //defining the size of each cell
     gridCell.style.minWidth = '40px';
     gridCell.style.minHeight = '40px';
     gridCell.style.maxWidth = '40px';
     gridCell.style.maxHeight = '40px';
+    gridCell.style.margin = '0px';
+    gridCell.style.padding = '0px';
 
     //gridCell.style.
     containerJS.appendChild(gridCell);
@@ -38,8 +49,4 @@ for(let i = 0; i <= 255; i++) {
         gridClass[i].style.backgroundColor = "red";
     });
     
-/*    gridClass2[i].addEventListener("mouseout", function() {
-        // Code to run when the mouse pointer exits the element (no longer hovered)
-        gridClass2[i].style.backgroundColor = ""; // Reset the background color
-    }); */
 }
