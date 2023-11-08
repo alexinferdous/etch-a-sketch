a3a5701bd2c3cb6c90c9f0535888c94b3c3b687b
Ferdous <71073170+alexinferdous@users.noreply.github.com>
2023-11-07 23:34:15 -0800

    Project Complete

--- javascript.js
+++ javascript.js
@@ -15,38 +15,57 @@ const top1 = (window.innerHeight - containerJS.offsetHeight) / 2;
 containerJS.style.left = left1 + 'px';
 containerJS.style.top = top1 + 'px';
 
-
 containerJS.style.display = "flex";
 containerJS.style.flexWrap = "wrap"; // or "nowrap" to prevent wrapping 
 
+function createGrid(cellNumber) {
 
-for(let i = 0; i <=255; i++){
-    // create 16x16 cells
-    const gridCell = document.createElement('div');
-
-    gridCell.setAttribute('id', `${i}`);
-    gridCell.classList.add("gridClass");
-    gridCell.setAttribute('style', `border:1px dotted #808080`);
+    for(let i = 0; i <cellNumber*cellNumber; i++){
+        const gridCell = document.createElement('div');
     
-    //defining the size of each cell
-    gridCell.style.minWidth = '40px';
-    gridCell.style.minHeight = '40px';
-    gridCell.style.maxWidth = '40px';
-    gridCell.style.maxHeight = '40px';
-    gridCell.style.margin = '0px';
-    gridCell.style.padding = '0px';
+        gridCell.setAttribute('id', `${i}`);
+        gridCell.classList.add("gridClass");
+        gridCell.setAttribute('style', `border:1px dotted #808080`);
+        
+        //defining the size of each cell
+        const cellSize = (672/cellNumber) - 2; //subtracting 2 because border is 1 px on each side
+        gridCell.style.minWidth = cellSize + 'px';
+        gridCell.style.minHeight = cellSize + 'px';
+        gridCell.style.maxWidth = cellSize + 'px';
+        gridCell.style.maxHeight = cellSize + 'px';
+        gridCell.style.margin = '0px';
+        gridCell.style.padding = '0px';
+    
+        containerJS.appendChild(gridCell);
+    }
+    
+    const gridClass = document.getElementsByClassName("gridClass");
+    
+    for(let i = 0; i <cellNumber*cellNumber; i++) {
+    
+        gridClass[i].addEventListener("mouseover", function() {
+            // Code to run when the mouse pointer enters the element (hovered)
+            const randomColor = getRandomColor();
+            gridClass[i].style.backgroundColor = randomColor;
+        });
+        
+    }
 
-    //gridCell.style.
-    containerJS.appendChild(gridCell);
 }
 
-const gridClass = document.getElementsByClassName("gridClass");
 
-for(let i = 0; i <= 255; i++) {
+createGrid(16);
 
-    gridClass[i].addEventListener("mouseover", function() {
-        // Code to run when the mouse pointer enters the element (hovered)
-        gridClass[i].style.backgroundColor = "red";
-    });
-    
-}
+const button = document.getElementById("btn");
+button.addEventListener("click", function(){
+    containerJS.innerHTML= '';
+    const userInput = prompt("Please enter the number of grids in one side: ")
+    createGrid(userInput);
+});
+
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+  }
